e4ee6649d2ccffff616345c2dbf4f569a34308dc
JackyShen <hgshen2013@sina.com>
2015-07-27 16:02:43 +0800

    Abandon mess files

--- app/src/main/java/com/jacky/myapplication/MainActivity.java
+++ app/src/main/java/com/jacky/myapplication/MainActivity.java
@@ -1,17 +1,12 @@
 package com.jacky.myapplication;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 
 public class MainActivity extends ListActivity {
